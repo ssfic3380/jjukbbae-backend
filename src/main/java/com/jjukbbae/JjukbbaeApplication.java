23268290f4f2a6294cbf2767611aab8ae90aa24e
hys <ssfic3380@gmail.com>
2024-11-02 23:08:57 +0900

    init

---
+++ src/main/java/com/jjukbbae/JjukbbaeApplication.java
@@ -0,0 +1,13 @@
+package com.jjukbbae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjukbbaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjukbbaeApplication.class, args);
+	}
+
+}
